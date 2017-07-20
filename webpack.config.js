1b7110ce8b1701d02506b698bc16b2bd00ffa7aa
yorthehunter <boru.design@gmail.com>
2017-07-20 17:08:04 -0400

    boilerplates a jekyll site; adds webpack, yarn; webpack_config; jekyll config; clear out some unecessaries; adds dependencies, including Weave; gitignore

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  // webpack folder’s entry js — excluded from jekll’s build process.
+  entry: './webpack/entry.js',
+  output: {
+    // we’re going to put the generated file in the assets folder so jekyll will grab it.
+    path: path.resolve(__dirname, 'src/assets/javascripts/'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }, {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }, {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          //resolve-url-loader may be chained before sass-loader if necessary
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [new ExtractTextPlugin("../../vendor.css")]
+};
---
+++ webpack/entry.js
@@ -0,0 +1,16 @@
+import '../node_modules/@stitch-fix/weave/scss/weave.scss'
+
+console.log("Hello, world!")
+
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import Hello from './components/Hello';
+
+class App extends Component {
+  render() {
+    return (<Hello/>)
+  }
+}
+
+render(
+  <App/>, document.getElementById('root'));
